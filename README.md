46c1a553e65de2d1906b23d9d431c3748066224c
Tobias Grothe <85501570+tgrothe@users.noreply.github.com>
2023-04-21 09:59:03 +0200

    Feature 1 (#1)
    * Feature 1
    
    * Prettified Code!
    
    * Update readme
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: Tobias Grothe <tgrothe@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ilias-disable-safe-all
+
 A tiny Tampermonkey JavaScript script, to disable the safe all buttons in ILIAS
+
+---
+
+Please see [ilias_script_tobias.js](ilias_script_tobias.js)
---
+++ ilias_script_tobias.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Script to disable the save all buttons
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Tobias
+// @match        https://www.hsbi.de/elearning/ilias.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hsbi.de
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  document
+    .querySelectorAll("input[name='cmd[saveStatusAll]']")
+    .forEach((btn) => {
+      btn.disabled = true;
+    });
+})();
